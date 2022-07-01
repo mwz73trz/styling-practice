e71f4cb8dc09aa30c80461ad19028c740ff4fb0f
mwz73trz <michaelzura@bellsouth.net>
2022-07-01 07:53:36 -0400

    initial frontend commmit

---
+++ frontend/src/api/contactAPI.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import apiHelpers from "./apiHelpers";
+
+const BASE_URL = "http://localhost:8000/api/";
+
+const contactAPI = {};
+
+contactAPI.login = async (loginData) => {
+  return await apiHelpers.tryCatchFetch(() =>
+    axios.post(`${BASE_URL}login/`, loginData, apiHelpers.getCsrfConfig())
+  );
+};
+
+contactAPI.logout = async () => {
+  return await apiHelpers.tryCatchFetch(() =>
+    axios.post(`${BASE_URL}logout/`, apiHelpers.getCsrfConfig())
+  );
+};
+
+contactAPI.signup = async (signupData) => {
+  return await apiHelpers.tryCatchFetch(() =>
+    axios.post(`${BASE_URL}signup/`, signupData, apiHelpers.getCsrfConfig())
+  );
+};
+
+contactAPI.getContacts = async () => {
+  return await apiHelpers.tryCatchFetch(() =>
+    axios.get(`${BASE_URL}contacts/`, apiHelpers.getCsrfConfig())
+  );
+};
+
+export default contactAPI;
